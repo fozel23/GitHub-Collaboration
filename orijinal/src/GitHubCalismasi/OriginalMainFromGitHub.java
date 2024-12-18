aefb49586fd04cf81111abee054bd8ab9e247780
fozel23 <fgoa23@gmail.com>
2024-12-18 19:49:06 +0300

    birinci calisma

---
+++ orijinal/src/GitHubCalismasi/OriginalMainFromGitHub.java
@@ -0,0 +1,10 @@
+package GitHubCalismasi;
+
+public class OriginalMainFromGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("6. satirda calisma yapildi");
+        System.out.println("7. satirda calisma yapildi");
+        System.out.println("8. satirda calisma yapildi");
+    }
+}
